a487987d3fd186b638d24ea73c95bb89d16fb6d4
n-vasenkin <vasenking@mail.ru>
2020-06-04 21:23:38 +0500

    Avatar error

--- src/ui-kit/Avatar/index.tsx
+++ src/ui-kit/Avatar/index.tsx
@@ -1,11 +1,28 @@
+import { action, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import React from 'react';
 import { Avatar, Img } from './style';
 import { AvatarProps } from './types';
 
-export default ({ image, size, className }: AvatarProps) => {
-  return (
-    <Avatar size={size} className={className}>
-      <Img src={image || require('./default.png')} alt="avatar" />
-    </Avatar>
-  );
-};
+@observer
+export default class UiAvatar extends React.Component<AvatarProps> {
+  @observable private err: boolean;
+
+  @action.bound
+  onError(): void {
+    this.err = true;
+  }
+
+  render(): JSX.Element {
+    const { image, size, className } = this.props;
+    return (
+      <Avatar size={size} className={className}>
+        <Img
+          alt="avatar"
+          onError={this.onError}
+          src={(this.err && require('./default.png')) || image || require('./default.png')}
+        />
+      </Avatar>
+    );
+  }
+}
